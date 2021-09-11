78028078e3cd3b3ccb1ee7efe3b0a3bae3c7cd4f
novakvova <novakvova@gmail.com>
2021-09-11 16:00:53 +0300

    Begin adrooid and ASP

---
+++ Web.Store/Data/EFAppContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Store.Data.Entities;
+
+namespace Web.Store.Data
+{
+    public class EFAppContext : DbContext
+    {
+        public EFAppContext(DbContextOptions<EFAppContext> options) :
+            base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+    }
+}
