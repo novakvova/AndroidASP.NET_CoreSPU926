e2b8d8536a99903ceb540494e3a65cc45c0b0f5e
novakvova <novakvova@gmail.com>
2021-10-09 18:05:04 +0300

    Cropper super girl

---
+++ anroind/app/src/main/java/com/example/androidstore/ProductAddActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_add);
+    }
+}
