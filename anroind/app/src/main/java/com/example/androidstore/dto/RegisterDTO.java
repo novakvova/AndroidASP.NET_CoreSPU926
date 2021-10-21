a5948c51eb6cbddd1ded94fe5732fef5c7819b7e
novakvova <novakvova@gmail.com>
2021-10-21 17:45:51 +0300

    fix register

--- anroind/app/src/main/java/com/example/androidstore/dto/RegisterDTO.java
+++ anroind/app/src/main/java/com/example/androidstore/dto/RegisterDTO.java
@@ -4,6 +4,12 @@ import lombok.Data;
 
 @Data
 public class RegisterDTO {
+
     private String email;
+    private String firstName;
+    private String secondName;
+    private String phone;
+    private String photo;
     private String password;
+    private String сonfirmPassword;
 }
